349293ec4f854945e6a253bb60b184a2b487c297
Epu-22 <126691333+Epu-22@users.noreply.github.com>
2023-03-04 22:52:00 -0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,14 +1,24 @@
 // Mostrar código QR
 
-document.getElementById("expandir").addEventListener("click", function() {
-    document.getElementById("oculto").style.display = "block";
+let presionar = document.getElementById("expandir");
+let visible = document.getElementById("oculto");
+
+presionar.addEventListener("click", function () {
+    if (visible.style.display == "block") {
+        visible.style.display = "none";
+        presionar.innerHTML = "Vincular con dispositivo móvil";
+    }
+    else {
+        visible.style.display = "block";
+        presionar.innerHTML = "Ocultar código QR";
+    }
 });
 
 
 
 // Efecto de desplazamiento
 
-const links = document.getElementsByClassName("boton");
+const links = document.getElementsByClassName("botones");
 
 for (const link of links) {
     link.addEventListener("click", clickHandler);
